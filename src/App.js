8e2fba316579b9523cb08dc732a3a9269fe8a562
Nick Hinchliffe <hinchliffen@wit.edu>
2021-03-29 17:21:17 -0400

    Simple button and background

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 
+const ViewOne = ({onClick}) => (
+  <div>
+    View 1 <br />
+    <button onClick={() => onClick("view2")}>Go to view 2</button>
+  </div>
+);
+
+const ViewTwo = ({onClick}) => (
+  <div>
+    View 2 <br />
+    <button onClick={() => onClick("view1")}>Go to view 1</button>
+  </div>
+);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Hello</button>
     </div>
   );
 }
